6d61b79582aadfebbb2bbb817a1c824adf1479cb
Nathan Leite <natetheaverage@gmail.com>
2016-04-03 05:34:52 -0600

    duh

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -2,13 +2,13 @@
 <head>
 </head>
 <body>
-    <h3><?php echo $m->to; ?></h3>
-    <h3><?php echo $m->from; ?></h3>
+    <h3><?php echo $m['sender']; ?></h3>
+    <h3><?php echo $m['from']; ?></h3>
     <p>Thank you for your message, we will get back to you very soon.
     This is the message we recieved:</p>
     <br />
-    <h3><?php echo $m->subject; ?></h3>
-    <p><?php echo $m->message; ?></p>
+    <h3><?php echo $m['subject']; ?></h3>
+    <p><?php echo $m['body-html']; ?></p>
 </body>
 </html>
 
