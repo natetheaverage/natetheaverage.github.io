211548d805e1e94b82ff85736bb4cef27e55c8ab
Nathan Leite <natetheaverage@gmail.com>
2016-04-03 05:37:06 -0600

    asdfg as

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -2,7 +2,7 @@
 <head>
 </head>
 <body>
-    <h3><?php echo $m['sender']; ?></h3>
+    <h3><?php echo $m['to']; ?></h3>
     <h3><?php echo $m['from']; ?></h3>
     <p>Thank you for your message, we will get back to you very soon.
     This is the message we recieved:</p>
