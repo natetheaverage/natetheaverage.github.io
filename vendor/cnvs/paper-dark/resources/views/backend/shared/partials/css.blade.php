28858707c0480b1a7cc388234bcae5875e8ade37
NateTheAverage <natetheaverage@gmail.com>
2017-05-30 06:26:46 -0600

    removed vondor form git cache

--- vendor/cnvs/paper-dark/resources/views/backend/shared/partials/css.blade.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/backend.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ elixir('vendor/canvas/assets/css/media-manager.css') }}">
--- vendor/cnvs/paper-dark/resources/views/frontend/emails/password.blade.php
+++
@@ -1,2 +0,0 @@
-<!-- resources/views/emails/password.blade.php -->
-Click here to reset your password: {{ route('canvas.auth.password.reset', $token) }}
