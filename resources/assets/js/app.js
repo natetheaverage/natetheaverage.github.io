0181f55b895ebfa85608da4aa16f3b1e314bfdf6
NateTheAverage <natetheaverage@gmail.com>
2017-05-31 19:50:37 -0600

    upgrading to axios for http requests

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,9 +3,11 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-import VueResource from 'vue-resource'
+// import VueResource from 'vue-resource'
 import Vue from 'vue'
-Vue.use(VueResource)
+import Axios from 'axios'
+// Vue.use(VueResource)
+Vue.prototype.$http = Axios;
 
 // enable @keyup.s
 // Vue.directive('on').keyCodes.s = 83
@@ -15,14 +17,6 @@ Vue.use(VueResource)
 //Vue.http.headers.common['Access-Control-Allow-Origin'] = 'http://natetheaverage.com';
 // document.querySelector('#token').getAttribute('value');
 
-// https://openapi.etsy.com/v2/users/9311200/shops?api_key=ljv61i2bg4wa3s9ho3egqgz7
-
-// Vue.http.get('/etsyLogin').then(data => {
-//         console.log(`app.js@ready - UserInfo`);
-//     },
-//     (response, status) => {
-//         //     console.log(`Err app.js@ready with - %s `, status)
-//     })
 
 
 
@@ -56,18 +50,19 @@ Vue.component('videoPlayer', VideoPlayer)
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 /
-// listings / active.js ? callback = getData & api_key = ljv61i2bg4wa3s9ho3egqgz7
 
 Vue.component('example', require('./components/Example.vue'));
 
 const etsyUrl = "https://openapi.etsy.com/v2";
 const etsyShopId = '6577223';
 const etsyUserId = '9311200';
-const app = Vue.extend({
-    // el: '#app-container',
+
+const app = new Vue({
+    el: '#app-container',
     data() {
         return {
-            etsyUserResource: this.$resource('https://openapi.etsy.com/v2/shops/6577223.js?callback=getData&api_key=ljv61i2bg4wa3s9ho3egqgz7'),
+            etsyData: {},
+            // etsyUserResource: this.$resource('https://openapi.etsy.com/v2/shops/6577223.js?callback=getData&api_key=ljv61i2bg4wa3s9ho3egqgz7'),
             // etsyShopResource: this.$resource(etsyUrl + '/shops/:etsyShop' + 'js?callback=getData&api_key=ljv61i2bg4wa3s9ho3egqgz7'),
             // etsyListingsResource: this.$resource(etsyUrl + '/shops/:etsyShop/listings/activejs?callback=getData&api_key=ljv61i2bg4wa3s9ho3egqgz7'),
         }
@@ -75,25 +70,30 @@ const app = Vue.extend({
     },
     mounted() {
         // Call api for collection of each model
-        console.log(`app.js@ready`);
-        this.etsyUserResource.get(data => {
-                console.log(`app.js@ready - UserInfo`);
-            },
-            (response, status) => {
-                //     console.log(`Err app.js@ready with - %s `, status)
-            })
+        //console.log(`vue mounted on ${ this }`);
+
+
+
+        // this.$http.get(getData => {
+        //         console.log(`app.js@ready - UserInfo ${ getData }`);
+        //         that.etsyData = data;
+        //     },
+        //     (response, status) => {
+        //         console.log(`Err app.js@ready with - %s `, status)
+        //     })
     },
 });
-app.use(VueResource);
-Vue.http.get("/etsyLogin",
-    (data) => console.log(data),
-    (errData) => console.log(errData)
-)
-console.log(Vue.http.get);
 
-let APP = new app({
-    el: '#app-container',
-});
+axios.get('https://openapi.etsy.com/v2/shops/6577223.js?callback=getData&api_key=ljv61i2bg4wa3s9ho3egqgz7')
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+
+
 
 //logo animation function
 const lanime = require("./svgAnime.js")
