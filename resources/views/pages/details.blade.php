9a07209e5c10dfc9a6b219e9e948a1154cde0e9f
NateTheAverage <NateTheAverage@gmail.com>
2017-09-20 13:18:03 -0600

    this fix?

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -2,4 +2,75 @@
 
 @section('content')
 
+​Skill Set
+This is a rated resume based on a five star rating one star representing absolute beginner, five stars representing perfection.
+ 
+Stilts  
+Four Stars / Peg, Spring, Articulating, & Arm. Very comfortable in any atmosphere and conditions. Can front flip on springs, and jump rope on pegs. Have stilted on ice, mud, rain. While doing four legged creatures Nate will commonly carry people on his back, he even has a saddle rigged for such occasions.
+ 
+ 
+Mascot/Statue
+Four stars / The key to any good caricature performance is confidence that you are that entity. With mascots and statues the audience already has an eye drawn towards Nate, he then just helps them suspend their disbelief.
+
+​Urban Dance​
+Three stars / Bboying (breakdance), Popping, House, Urban Freestyles. Of all his performance skills dance is the one he has trained the most and still sees tons of room for improvement.
+
+​Acting / Clowning​
+Four stars / Avant Garde, Caricature, Traditional, Mime, Movie, Commercial, On stage or Crowd roaming Nate has been able to embody the essence of any caricature he is given the part.
+
+​Makeup​
+Two Stars / Custom foam latex appliances, clown, Complete caricature, Design, Creation, Apply. Nate has a lot of progress to make in this area, he is a novise with an airbrush and feels that is a big key to great makeup.
+
+​Costume Fabrication​
+Four stars / Mascot, Furies, Stilt, Dance ware. Sewing, Foam Carving, Hair Laying, LED, EL Wire, Extended Limbs, Props (working and decorative), plastic forming. Any dream can come to reality Nate loves making it so.
+
+
+Who: THE BOX NYC & London / VVWorldwide
+What: Performer / Costume Designer / Asosiate Director
+Where: New York City NY. & SOHO London
+When: 2013 - Present
+Notes: Depravity becomes EVERYONE!
+ 
+​Who: ESQUE @ DBA Hollywood / VVWorldwide
+What: Performer / Costume Designer / Asosiate Director
+Where: Hollywood Ca.
+When: October 2013 - March 2014
+Notes: Wrote, co-directed and performed a temparary show at a new club in hollywood to help its grand opening. ESQUE was extended three extra months after its great success. I stayed on two months after as the clubs art director.
+ 
+​Who: The Act Las Vegas / VVWorldwide
+What: Performer / Costume Designer / Asosiate Director
+Where: Las Vegas NV.
+When: October 2012 - October 2013
+Notes: Depravity becomes me. The Act was shut down in October of 2013 for being to raunchy for vegas.
+﻿
+Who: INSOMNIAC
+What: Stilt Walker / Mascot / Costume Design / Actor
+Where: Internationaly
+When: 2008 - Present
+Notes: Nate has traveled with them internationally for multiple music festivals Including Electric Daisy Carnival, Nocturnal Wonderland, Beyond Wonderland. Has also Created specialty costumes for  these events and acted in several commercials for the events.
+​
+Who: ULTRA MUSIC FESTIVAL
+What: Stilt Walker / Mascot / Costume Design
+Where: Miami FL.
+When: 2012 - Present
+Notes: Created and performed ULTRA Bot  Mascots
+﻿
+Who: ELITCH GARDENS/ SIX FLAGS
+What: Stilt walker, Stunt Man, Dancer, Fire Performer
+Where: Denver Co.
+When: 2005 - 2007
+Notes: Carnival de Fuego evening fire show. Stilt walker on articulating & four legged. X-City parkour break dance stunt show. Fright Fest Halloween roaming stilt walker.
+﻿
+- The Brit Awards 2014
+- Tronto Film Festival 2013
+- Sundance Film Feastival 2013
+- WILDCHILD WORLD 2012
+﻿- Global Dance Festival 2006 - 2011
+﻿- Team EZ performance troupe 2006 -2011
+- Church & Vinyl nightclubs 2002 - 2006
+- Denver Nuggets D-boys 2005
+- Kevin Larson Presents 2006- 2011
+- Burning Man (art community festival) 2008 - 2010
+- Street Performer (nationwide) 2011 -Present﻿
+
 @stop
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -2,4 +2,54 @@
 
 @section('content')
 
+“Mirror Man, exactly what he sounds like and a festival staple” – Billboard Magazine​
+​
+​
+Day Rate – $500
+Hourly Rate – $200 the ﬁrst hour ($100/each additional hour)
+
+
+​Stilt Walking
+(2 OR 4 LEG/ARM STILTING)
+Daily Rate – $500
+Hourly Rate – $200 the ﬁrst hour
+$150 each additional hour 
+
+​Stiltng Assistant (costumed girl on saddle)
+Day Rate - $250 
+Hourly Rate - $100 ﬁrst hour
+$75 each additonal hour
+ 
+​Mascot (Dog, large Robot, etc.)
+Day Rate - $400
+Hourly Rate - $200 first hour
+$100 each additional 
+
+
+​COSTUME (Design, Fabrication)
+ 
+As a costume designer, NateTheAverage is able to take your concept, idea or inspiration and make it a reality.
+Costume/Mascot creaton starts at $750 (goes up depending
+on materials extra time)
+Costume rental $150 a day (lower for minimal costumes: clown outfit, just clothing, etc)
+He is skilled in:
+Make up special fx
+Foam latex prosthetic appliances
+Life casting
+Sewing
+Head dresses
+Masks
+Foam carving
+Raw LED wiring
+LED system programing
+EL wire placement
+Prop fabrication
+Pneumatic systems
+Wigs
+Plastic forming
+Mascot creation
+Fur
+Feathers
+Specialty Designs
+
 @stop
