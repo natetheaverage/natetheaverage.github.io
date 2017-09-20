fab18c76f4621cedb6f81fa7ab72f55d0eff52b8
NateTheAverage <NateTheAverage@gmail.com>
2017-09-20 13:40:56 -0600

    jhg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
       <main-menu></main-menu>
       </div> 
     </div>
-    @include('vendor.canvas.frontend.shared.footer')
+    @include('vendor.canvas.frontend.shared.partials.footer')
     
     <script type="text/javascript" src="js/app.js"></script>
     {{-- <script src="http://vjs.zencdn.net/5.19.2/video.js"></script> --}}
