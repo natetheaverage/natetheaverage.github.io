4975b6931e8fbefbbcff8cdf1e729b82e7dfa5c6
NateTheAverage <NateTheAverage@gmail.com>
2017-09-23 01:09:32 -0600

    kjh

--- resources/views/vendor/canvas/frontend/layout.blade.php
+++ resources/views/vendor/canvas/frontend/layout.blade.php
@@ -2,12 +2,13 @@
 <html lang="en">
     <head>
         @include('includes.head')
+        <link href="css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
       <div id="app-container">
         @include('canvas::frontend.shared.partials.header')
         @yield('content')
-
+ 
       </div>
         @yield('unique-js')
         @include('canvas::frontend.shared.partials.user-generated-js')
