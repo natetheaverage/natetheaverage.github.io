b956c4a45aeb51d03a354cb9105b7a75e2c3f692
NateTheAverage <NateTheAverage@gmail.com>
2017-09-25 04:13:09 -0600

    wip

--- resources/views/parts/main-links.blade.php
+++ resources/views/parts/main-links.blade.php
@@ -6,7 +6,7 @@
 @else
 <a href="{!! route('nta.store') !!}" class="page-menu-links ">Shop</a>
 @endif
-@if( Request::is('services/*'))
+@if( Request::is('services') || Request::is('services/*'))
 <a href="{!! route('nta.services') !!}" class="page-menu-links active">Services</a>
 @else
 <a href="{!! route('nta.services') !!}" class="page-menu-links">Services</a>
@@ -21,7 +21,7 @@
 @else
 <a href="{!! route('nta.fun') !!}" class="page-menu-links">Fun</a>
 @endif
-@if( Request::is('services/*'))
+@if( Request::is('services'))
 <div class="container-fluid" id="head-c">
     <div class="row">
         <p class="page-menu-links-container mgn-btm-lg col-sm-8 col-sm-offset-2">
