7f5af1f9d8118afc8993bab898c945eac30f3067
NateTheAverage <NateTheAverage@gmail.com>
2017-09-25 05:17:58 -0600

    wip

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -15,8 +15,8 @@
   <link href="https://fonts.googleapis.com/css?family=Raleway:100,200" rel="stylesheet" type="text/css">
 
   <!-- js video player   -->
-  {{--  <link href="//vjs.zencdn.net/5.19/video-js.min.css" rel="stylesheet">
-  <script src="path/to/videojs-chromecast/dist/videojs-chromecast.js"></script>  --}}
+  <link href="//vjs.zencdn.net/5.19/video-js.min.css" rel="stylesheet">
+  
    
    <!-- Favicon -->
   <link rel="icon" type="image/png" href="{{ asset('/assets/images/nta-favicon.png') }}">
