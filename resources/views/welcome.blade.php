8411f1e1854e6467443ebe3292c6f30a0b7ddad0
NateTheAverage <NateTheAverage@gmail.com>
2017-09-25 05:34:43 -0600

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
     <script type="text/javascript" src="js/app.js"></script>
     {{-- <script src="http://vjs.zencdn.net/5.19.2/video.js"></script> --}}
     <script src="../node_modules/video.js/dist/video.min.js"></script>
-    <script src="../dist/Youtube.min.js"></script>
+    <script src="../node_modules/videojs-youtube/dist/Youtube.min.js"></script>
     <script src="../node_modules/videojs-chromecast/dist/videojs-chromecast.js"></script>
 
     
